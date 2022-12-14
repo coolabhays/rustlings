b9611b94bace4f7e7a429b01ed59bde1737e0556
coolabhays <abhaysp9955@gmail.com>
2022-12-14 18:29:33 +0530

    feat: done with quiz1 (first part complete)

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,22 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    // this works
+    /* let mut total_price = 0;
+    if quantity > 40 {
+        // total_price += (40 * 2) + (quantity - 40);  // I think extra sometimes
+        total_price += quantity;
+    } else {
+        total_price += (quantity * 2);
+    }
+
+    total_price */
+
+    // ternary approach
+    if quantity > 40 { quantity} else { quantity * 2 }
+}
 
 // Don't modify this function!
 #[test]
