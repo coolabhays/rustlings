1971d02eaee46834bde30e9f4ba39aa889cbf711
coolabhays <abhaysp9955@gmail.com>
2022-12-16 20:21:46 +0530

    feat: completed rustlings on move_semantics

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,16 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut x = 100;
-    let y = &mut x;
+    /* let y = &mut x;  // you can't have to mutable bindings, but
     let z = &mut x;
     *y += 100;
+    *z += 1000; */
+    let y = &mut x;  // reference y is used before creating another mutable reference (if you'll
+                     // use y after making z, you'll get error
+    *y += 100;
+    let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
 }
