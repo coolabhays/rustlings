4289901bdbe88526de1d8b7a23511a93f5ee15d8
coolabhays <abhaysp9955@gmail.com>
2022-12-22 22:23:38 +0530

    feat: rustlings done for hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,6 +38,46 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        /* *scores.entry(team_1_name).or_insert(Team {
+            name: team_1_name,
+            goals_scored: team_1_score,
+            goals_conceded: team_2_score,
+        });
+        *scores.entry(team_2_name).or_insert(Team {
+            name: team_2_name,
+            goals_scored: team_2_score,
+            goals_conceded: team_1_score,
+        }); */
+        // is there a way to do this in short with 'entry' ?
+
+        match scores.get_mut(&team_1_name) {
+            Some(team) => {
+                team.goals_scored += team_1_score;  // team is of &mut type, but I think it auto
+                                                    // dereferenced here
+                team.goals_conceded += team_2_score;
+            },
+            None =>  {
+                scores.insert(team_1_name.clone(), Team {
+                    name: team_1_name,
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                });
+            }
+        }
+        match scores.get_mut(&team_2_name) {
+            Some(team) => {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            },
+            None =>  {
+                scores.insert(team_2_name.clone(), Team {
+                    name: team_2_name,
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                });
+            }
+        }
     }
     scores
 }
